500c650ef156c365ea33dd740a14a596a58d5588
Joel Irineu <irineu.joel01@gmail.com>
2020-06-26 18:23:30 -0300

    criando componetes

--- App.js
+++ App.js
@@ -3,24 +3,34 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 
 class App extends Component{
   render(){
-    let nome = 'Irineu'
-    let img = 'https://bit.ly/2VmtlrM'
-
     return (
       <View>
         <Text>Olá mundo</Text>
         <Text>Meu primeiro App</Text>
         <Text style={{color: '#ff0000', fontSize: 25, margin: 10}}>Sujeito Programador</Text>
 
-        <Image 
-          source={{uri: img}}
-          style={{width: 300, height: 300}}
-        />
-
-        <Text style={{fontSize: 30,}}>{nome}</Text>
+        <Irineu alt={320} larg={200} fulano='Irineu' />
       </View>
     );
-    }
+  }
+}
+
+class Irineu extends Component{
+  render(){
+    let nome = this.props.fulano
+    let img = 'https://bit.ly/2VmtlrM'
+
+    return(
+      <View>
+        <Image 
+          source={{uri: img}}
+          style={{width: this.props.alt, height:this.props.larg}}
+        />
+
+        <Text style={{fontSize: 30, textAlign: 'center'}}>{nome}</Text>
+      </View>
+    )
+  }
 }
 
 export default App;
