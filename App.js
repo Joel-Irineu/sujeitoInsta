0d8413f880cd38a251fcd02c7fe44e4adc4c7e59
Joel Irineu <irineu.joel01@gmail.com>
2020-06-26 18:08:22 -0300

    class

--- App.js
+++ App.js
@@ -1,33 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
-export default function App() {
+class App extends Component{
+  render(){
+    let nome = 'Irineu'
+    let img = 'https://bit.ly/2VmtlrM'
 
-  let nome = 'Irineu'
-  let img = 'https://bit.ly/2VmtlrM'
+    return (
+      <View>
+        <Text>Olá mundo</Text>
+        <Text>Meu primeiro App</Text>
+        <Text style={{color: '#ff0000', fontSize: 25, margin: 10}}>Sujeito Programador</Text>
 
-  return (
-    <View>
-      <Text>Olá mundo</Text>
-      <Text>Meu primeiro App</Text>
-      <Text style={{color: '#ff0000', fontSize: 25, margin: 10}}>Sujeito Programador</Text>
+        <Image 
+          source={{uri: img}}
+          style={{width: 300, height: 300}}
+        />
 
-      <Image 
-        source={{uri: img}}
-        style={{width: 300, height: 300}}
-      />
-
-      <Text style={{fontSize: 30,}}>{nome}</Text>
-    </View>
-  );
+        <Text style={{fontSize: 30,}}>{nome}</Text>
+      </View>
+    );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
+export default App;
