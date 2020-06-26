8d25f0008f5fed541359a42a1c472bc89788ea88
Joel Irineu <irineu.joel01@gmail.com>
2020-06-26 18:39:44 -0300

    grupos de estilo

--- App.js
+++ App.js
@@ -1,35 +1,33 @@
 import React, { Component } from 'react';
-import { Text, View, Button } from 'react-native';
+import { Text, View, Button, StyleSheet} from 'react-native';
 
 class App extends Component{
-  
-  constructor(props){
-    super(props)
-    this.state ={
-      nome: 'joel'
-    }
-    this.entrar = this.entrar.bind(this)
-  }
 
-  entrar(nome){
-    this.setState({
-      nome: nome
-    })
-  }
   
   render(){
     return (
-      <View style={{marginTop: 20}}>
-        <Text style={{fontSize: 25, color: 'red', textAlign: 'center'}}>{this.state.nome}</Text>
-
-        <Button
-          title='Entrar'
-          onPress={()=>this.entrar('irineu')}
-        />
+      <View style={styles.container}>
+        <Text style={[styles.primaryText, styles.centerAlign]}>Eu sou o texto 1</Text>
+        <Text style={styles.centerAlign}>Eu sou o texto 2</Text>
+        <Text>Eu sou o texto 3</Text>
+        <Text style={styles.primaryText}>Eu sou o texto 4</Text>
 
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container:{
+    marginTop: 40
+  },
+  primaryText:{
+    fontSize: 25,
+    color: '#ff0000'
+  },
+  centerAlign:{
+    textAlign: 'center'
+  }
+})
 
 export default App;
